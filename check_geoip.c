985cf9d2ace482a150bcdbf8697f5df3f6e8dcd6
Tomas Mozes <tmozes@sygic.com>
2015-01-14 13:26:14 +0100

    Test country

--- check_geoip.c
+++ check_geoip.c
@@ -1,23 +1,39 @@
 #include "GeoIP.h"
 
 #define GEOIP_PATH "/usr/share/GeoIP"
+
+#define TEST_IP "8.8.8.8"
+#define TEST_COUNTRY "US"
+
 #define NAGIOS_OK 0
 #define NAGIOS_ERROR 1
 
+int print_status(char *text, int ret) {
+  printf("%s\n", text);
+
+  exit(ret);
+}
+
 int main(int argc, char **argv) {
   GeoIP *gi;
-  
+  const char *returned_country;
+
   gi = GeoIP_open(GEOIP_PATH "/GeoIP.dat", GEOIP_STANDARD);
 
-  if (gi != NULL) {
-    printf("OK");
-    GeoIP_delete(gi);
-    
-    return (NAGIOS_OK);
-  } else {
-    printf("No database found");
-    
-    return (NAGIOS_ERROR);
+  if (gi == NULL)
+    print_status("CRITICAL: cannot open database", NAGIOS_ERROR); 
+  
+  returned_country = GeoIP_country_code_by_addr(gi, TEST_IP);
+
+  if(returned_country == NULL)
+    print_status("CRITICAL: cannot query database", NAGIOS_ERROR); 
+
+  if(strcmp(returned_country, TEST_COUNTRY) != 0) {
+    print_status("CRITICAL: country query returned different values", NAGIOS_ERROR); 
   }
- 
+
+  GeoIP_delete(gi);
+  print_status("OK", NAGIOS_OK);
+
+  return 0;
 }
