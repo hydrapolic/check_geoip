872a0fc373965c5e0eba31ebd83216489c0ce4d4
Tomas Mozes <tmozes@sygic.com>
2015-01-15 12:25:04 +0100

    Fix bad return values of WARNING state

--- check_geoip.c
+++ check_geoip.c
@@ -223,7 +223,7 @@ int main(int argc, char **argv) {
   if(db_age > db_age_critical) {
     print_status(STATE_CRITICAL, "CRITICAL: database age = %d days, query for IP %s returned country %s | db_age=%d", db_age, test_ip, returned_country, db_age);
   } else if (db_age > db_age_warning) {
-    print_status(STATE_CRITICAL, "WARNING: database age = %d days, query for IP %s returned country %s | db_age=%d", db_age, test_ip, returned_country, db_age);
+    print_status(STATE_WARNING, "WARNING: database age = %d days, query for IP %s returned country %s | db_age=%d", db_age, test_ip, returned_country, db_age);
   } else {
     print_status(STATE_OK, "OK: database age = %d days, query for IP %s returned country %s | db_age=%d", db_age, test_ip, returned_country, db_age);
   }
